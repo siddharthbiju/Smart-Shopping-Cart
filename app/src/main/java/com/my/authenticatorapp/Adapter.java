2df62452d395f686418a698336789384bb38a62a
Sachit Anand <sachitanand78@gmail.com>
2021-05-19 22:14:46 +0530

    Cart 75%

--- app/src/main/java/com/my/authenticatorapp/Adapter.java
+++ app/src/main/java/com/my/authenticatorapp/Adapter.java
@@ -1,11 +1,21 @@
 package com.my.authenticatorapp;
 
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.network.ResumableUploadStartRequest;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +28,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private LayoutInflater layoutInflater;
     private List<String> data;
     private List<String> data2;
+    private StorageReference SR = FirebaseStorage.getInstance().getReference();
 
 
     Adapter(Context context, List<String> data, List<String> data2){
@@ -39,6 +50,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         String title = data.get(i);
         String desc = data2.get(i);
+        StorageReference itemref = SR.child("Items/"+title+".jpg");
         if(title == "Total"){
             viewHolder.textTitle.setText(title+" = "+desc);
             viewHolder.textDescription.setVisibility(View.INVISIBLE);
@@ -49,6 +61,17 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             viewHolder.img.setVisibility(View.VISIBLE);
             viewHolder.textTitle.setText(title);
             viewHolder.textDescription.setText(desc);
+            itemref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri1) {
+                    Picasso.get().load(uri1).into(viewHolder.img);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull @NotNull Exception e) {
+                    Log.d("Output","Failed to load");
+                }
+            });
         }
     }
 
--- app/src/main/java/com/my/authenticatorapp/Cart.java
+++ app/src/main/java/com/my/authenticatorapp/Cart.java
@@ -10,26 +10,34 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class Cart extends AppCompatActivity {
+    String cid="FF 19";
     RecyclerView recyclerView;
     Adapter adapter;
     ArrayList<String> items;
     ArrayList<String> desc;
+    ArrayList<String> rfid;
     FirebaseFirestore db= FirebaseFirestore.getInstance();
+    FirebaseFirestore db1= FirebaseFirestore.getInstance();
     CollectionReference dbs = db.collection("Items");
+    CollectionReference dbss = db1.collection("Cart/CartID/"+cid);
     SwipeRefreshLayout refreshLayout;
-    int s3,total;
-    String s1,s2;
+    int s3,total,k;
+    String s1,s2,s5,s4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,7 @@ public class Cart extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
         items = new ArrayList<>();
         desc = new ArrayList<>();
+        rfid = new ArrayList<>();
         refreshLayout = findViewById(R.id.refresh);
 
 
@@ -49,6 +58,7 @@ public class Cart extends AppCompatActivity {
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                rfid.clear();
                 showData();
                 refreshLayout.setRefreshing(false);
             }
@@ -57,31 +67,58 @@ public class Cart extends AppCompatActivity {
     }
     public void showData() {
 
+        dbss.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task1) {
+                for (DocumentSnapshot snap : task1.getResult()){
+                    s4 = snap.getString("Value");
+                    Log.d("output","rfid value = "+s4);
+                    rfid.add(s4);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                Toast.makeText(Cart.this, "Oops ... something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         db.collection("Items").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task1) {
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         items.clear();
                         desc.clear();
                         total = 0;
-                        for (DocumentSnapshot snapshot : task1.getResult()){
-                            s1 = snapshot.getString("Name");
-                            s2 = snapshot.getString("Price");
-                            s3=Integer.parseInt(s2);
-                            total = total+s3;
-                            items.add(s1);
-                            desc.add("Rs."+s2);
+                        for (k = 0 ; k < rfid.size() ; k++)
+                        {
+                            s4 = rfid.get(k);
+                            for (DocumentSnapshot snapshot : task.getResult()) {
+                                s1 = snapshot.getString("Name");
+                                s2 = snapshot.getString("Price");
+                                s5 = snapshot.getString("RFID value");
+                                if (s4.equals(s5)){
+                                    s3 = Integer.parseInt(s2);
+                                    total = total + s3;
+                                    items.add(s1);
+                                    desc.add("Rs." + s2);
+                                }
 
+                            }
                         }
                         items.add("Total");
                         desc.add("Rs." + total);
                         adapter.notifyDataSetChanged();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Cart.this, "Oops ... something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Cart.this, "Oops ... something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+         }
+
     }
-}
+
+
